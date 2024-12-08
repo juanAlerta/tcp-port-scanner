c2985a7c61a72bca7c99af12aedf98e4a92b7a61
juan <metridxblack@gmail.com>
2024-12-08 14:25:25 +0100

    cambios en módulos

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
-use tokio::net::TcpStream;
-use tokio::time::{timeout, Duration};
+mod scanner;
 
 #[tokio::main]
 async fn main() {
     let ip = "scanme.org"; // IP a escanear
-    let ports = 1..100; // Rango de puertos a probar
 
-    for port in ports {
-        let address = format!("{}:{}", ip, port);
-
-        // Cada escaneo es una tarea asincrónica
-        let scan = async move {
-            match timeout(Duration::from_secs(2), TcpStream::connect(&address)).await {
-                Ok(Ok(_)) => println!("Puerto abierto: {}", port),
-                Ok(Err(_)) | Err(_) => (), // Cerrado o sin respuesta
-            }
-        };
-
-        // Lanza la tarea
-        tokio::spawn(scan);
-    }
-
-    // Espera a que todas las tareas finalicen
-    tokio::time::sleep(Duration::from_secs(5)).await;
+    // Llamada a la función
+    scanner::scan_ports(ip).await;
 }
