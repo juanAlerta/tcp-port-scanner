fefc590bf8ad2291b13c4fefc2508bb00143f89a
juanAlerta <juan_operador@proton.me>
2024-12-13 16:47:52 +0100

    clap added

--- target/debug/tcp-port-scanner.d
+++ target/debug/tcp-port-scanner.d
@@ -1,1 +1,1 @@
-C:\Users\metri\source\tcp-port-scanner\target\debug\tcp-port-scanner.exe: C:\Users\metri\source\tcp-port-scanner\src\main.rs C:\Users\metri\source\tcp-port-scanner\src\scanner.rs
+C:\Users\juan\Projects\tcp-port-scanner\target\debug\tcp-port-scanner.exe: C:\Users\juan\Projects\tcp-port-scanner\src\main.rs C:\Users\juan\Projects\tcp-port-scanner\src\scanner.rs
